23146f00d1cb8c54f4759e4d72b61a541455fa75
Shashwat803 <shashwatmishra803@gmail.com>
2023-11-06 16:16:43 +0530

    updated env

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 require('dotenv').config()
 const sequelize = new Sequelize(process.env.DBNAME,process.env.DBUSERNAME, process.env.DBPASSWORD,{
-    host:process.env.DBHOST ,// has to db
+    host:process.env.DBHOST ,
     dialect:process.env.DIALECT
 })
 
